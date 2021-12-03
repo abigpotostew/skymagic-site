267efb4205eda76bfd2ba4bb129e0b02445b9989
stewart-prysm <stew.bracken@gmail.com>
2021-12-03 08:58:18 -0800

    site

---
+++ src/math.ts
@@ -0,0 +1,9 @@
+/**
+ * normalized sin, range shifted to [0..1]
+ */
+export const nsin = (n:number)=>(Math.sin(n)+1)/2
+
+/**
+ * normalized sin, range shifted to [0..1]
+ */
+export const ncos = (n:number)=>(Math.cos(n)+1)/2
